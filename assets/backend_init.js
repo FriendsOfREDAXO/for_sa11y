aba4bce0cf55ca95963e5d1e76336fcc153008d6
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 13:18:58 +0100

    Create backend_init.js

---
+++ assets/backend_init.js
@@ -0,0 +1,8 @@
+<script>
+  Sa11y.Lang.addI18n(Sa11yLangEn.strings);
+  const sa11y = new Sa11y.Sa11y({
+    customChecks: new CustomChecks, // Optional
+    checkRoot: ".cke5-editor",
+    containerIgnore: '.rex-minibar',
+  });
+</script>  
