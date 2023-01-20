819ccdc84ab82d077357825120726811ff7d8530
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 13:21:17 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,8 +1,9 @@
 <?php
+$addon = rex_addon::get('for_sa11y');
 if (rex::isBackend() && is_object(rex::getUser())) {
-    rex_view::addCssFile($plyr->getAssetsUrl('dist/css/sa11y.min.css'));
-    rex_view::addJsFile($plyr->getAssetsUrl('sa11y.umd.min.js'));   
-    rex_view::addJsFile($plyr->getAssetsUrl('dist/js/lang/en.umd.js'));
-    rex_view::addJsFile($plyr->getAssetsUrl('dist/js/sa11y-custom-checks.umd.min.js'));
-    rex_view::addJsFile($plyr->getAssetsUrl('backend_init.js'));
+    rex_view::addCssFile($addon->getAssetsUrl('dist/css/sa11y.min.css'));
+    rex_view::addJsFile($addon->getAssetsUrl('sa11y.umd.min.js'));   
+    rex_view::addJsFile($addon->getAssetsUrl('dist/js/lang/en.umd.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('dist/js/sa11y-custom-checks.umd.min.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('backend_init.js'));
 }
