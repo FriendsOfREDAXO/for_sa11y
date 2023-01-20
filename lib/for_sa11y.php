a4cc0fac0a4ad3bc3e3e7ad4618ed297245c971d
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 14:01:53 +0100

    Update for_sa11y.php

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -1,21 +1,12 @@
 <?php
 class for_sa11y
 {
-
- $addon = rex_addon::get('for_sa11y');
-if (rex::isBackend() && is_object(rex::getUser())) {
-    rex_view::addCssFile($addon->getAssetsUrl('dist/css/sa11y.min.css'));
-    rex_view::addJsFile($addon->getAssetsUrl('dist/js/sa11y.umd.min.js'));   
-    rex_view::addJsFile($addon->getAssetsUrl('dist/js/lang/en.umd.js'));
-    rex_view::addJsFile($addon->getAssetsUrl('dist/js/sa11y-custom-checks.umd.min.js'));
-    rex_view::addJsFile($addon->getAssetsUrl('backend_init.js'));
-
-
     /**
      * @return string
      */
     public static function get(): string
     {
+      $addon = rex_addon::get('for_sa11y');
 $js = '      
       <link rel="stylesheet" href="'.$addon->getAssetsUrl('dist/css/sa11y.min.css').'"/>
       <script src="'.$addon->getAssetsUrl('dist/js/sa11y.umd.min.js').'"></script>
