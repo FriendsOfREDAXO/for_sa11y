3a137f938628a748a6e70fee16412b211ea89cc1
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 13:17:07 +0100

    Create boot.php

---
+++ boot.php
@@ -0,0 +1,6 @@
+if (rex::isBackend() && is_object(rex::getUser())) {
+    rex_view::addCssFile($plyr->getAssetsUrl('dist/css/sa11y.min.css'));
+    rex_view::addJsFile($plyr->getAssetsUrl('sa11y.umd.min.js'));   
+    rex_view::addJsFile($plyr->getAssetsUrl('dist/js/lang/en.umd.js'));
+    rex_view::addJsFile($plyr->getAssetsUrl('dist/js/sa11y-custom-checks.umd.min.js'));
+}
