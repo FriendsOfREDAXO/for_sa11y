b016c5fe086389ff3aa9f3090c11f1a166ed0612
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 14:03:08 +0100

    Update for_sa11y.php

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -8,11 +8,11 @@ class for_sa11y
     {
       $addon = rex_addon::get('for_sa11y');
 $js = '      
-      <link rel="stylesheet" href="'.$addon->getAssetsUrl('dist/css/sa11y.min.css').'"/>
-      <script src="'.$addon->getAssetsUrl('dist/js/sa11y.umd.min.js').'"></script>
-      <script src="'.$addon->getAssetsUrl('dist/js/lang/en.umd.js').'"></script>
+      <link rel="stylesheet" href="'.$addon->getAssetsUrl("dist/css/sa11y.min.css").'"/>
+      <script src="'.$addon->getAssetsUrl("dist/js/sa11y.umd.min.js").'"></script>
+      <script src="'.$addon->getAssetsUrl("dist/js/lang/en.umd.js").'"></script>
 
-       <script src="'.$addon->getAssetsUrl('dist/js/sa11y-custom-checks.umd.min.js').'"></script>
+       <script src="'.$addon->getAssetsUrl("dist/js/sa11y-custom-checks.umd.min.js").'"></script>
   Sa11y.Lang.addI18n(Sa11yLangEn.strings);
   const sa11y = new Sa11y.Sa11y({
     customChecks: new CustomChecks, // Optional
