5793235a170066ab31741d7fe640b0f5fc0ac19c
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-20 20:46:49 +0100

    Create for_sa11y.php

---
+++ lib/for_sa11y.php
@@ -0,0 +1,26 @@
+<?php
+class for_sa11y
+{
+  /**
+   * @return string
+   */
+  public static function get(): string
+  {
+    $addon = rex_addon::get('for_sa11y');
+    $js = '      
+      <link rel="stylesheet" href="' . $addon->getAssetsUrl("dist/css/sa11y.min.css") . '"/>
+      <script src="' . $addon->getAssetsUrl("dist/js/sa11y.umd.min.js") . '"></script>
+      <script src="' . $addon->getAssetsUrl("dist/js/lang/en.umd.js") . '"></script>
+       <script src="' . $addon->getAssetsUrl("dist/js/sa11y-custom-checks.umd.min.js") . '"></script>
+  <script>     
+  Sa11y.Lang.addI18n(Sa11yLangEn.strings);
+  const sa11y = new Sa11y.Sa11y({
+    customChecks: new CustomChecks, // Optional
+    checkRoot: "body",
+    containerIgnore: \'.rex-minibar\',
+  });
+</script>        
+';
+    return $js;
+  }
+}
