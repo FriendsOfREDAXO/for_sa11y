287237e5f1d23823f37681255b93bf4af6c57f87
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-21 20:22:46 +0100

    Delete backend_init.js

--- assets/backend_init.js
+++
@@ -1,6 +0,0 @@
-  Sa11y.Lang.addI18n(Sa11yLangEn.strings);
-  const sa11y = new Sa11y.Sa11y({
-    customChecks: new CustomChecks, // Optional
-    checkRoot: "#rex-js-main-content",
-    containerIgnore: '.rex-minibar,.panel-heading',
-  });
