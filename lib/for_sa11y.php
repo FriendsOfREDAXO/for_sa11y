843627f43fe2eac98f75ceb7ebd7fd41ce7adf03
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-27 15:19:18 +0100

    added custom German build

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -9,7 +9,7 @@ class for_sa11y
       $js = '      
       <link rel="stylesheet" href="' . $addon->getAssetsUrl("dist/css/sa11y.min.css") . '"/>
       <script src="' . $addon->getAssetsUrl("dist/js/sa11y.umd.min.js") . '"></script>
-      <script src="' . $addon->getAssetsUrl("dist/js/lang/en.umd.js") . '"></script>
+      <script src="' . $addon->getAssetsUrl("dist/js/lang/de.umd.js") . '"></script>
        <script src="' . $addon->getAssetsUrl("dist/js/sa11y-custom-checks.umd.min.js") . '"></script>
   <script>     
   Sa11y.Lang.addI18n(Sa11yLangDe.strings);
