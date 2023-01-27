a8c3eab1e157c0a12d40fe8213b46168da55abb8
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-27 23:30:42 +0100

    Update for_sa11y.php

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -22,14 +22,14 @@ class for_sa11y
       $js = '      
       <link rel="stylesheet" href="' . $addon->getAssetsUrl("dist/css/sa11y.min.css") . '"/>
       <script src="' . $addon->getAssetsUrl("dist/js/sa11y.umd.min.js") . '"></script>
-      <script src="' . $addon->getAssetsUrl("dist/js/lang/de.umd.js") . '"></script>
+      <script src="' . $addon->getAssetsUrl("dist/js/lang/".$lang['js'].".umd.js") . '"></script>
        <script src="' . $addon->getAssetsUrl("dist/js/sa11y-custom-checks.umd.min.js") . '"></script>
   <script>     
   Sa11y.Lang.addI18n(Sa11yLang'.$lang["setup"].'.strings);
   const sa11y = new Sa11y.Sa11y({
     customChecks: new CustomChecks,
     checkRoot: \'body\',
-    readabilityLang:	\''.$lang["js"].'\',
+    readabilityLang:	\'de\',
     containerIgnore: \'.rex-minibar,.sa11y-ignore\',
   });
 </script>        
