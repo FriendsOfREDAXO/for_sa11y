3381860cdaa6fc93eb3969c5d174f4d4c1df23d5
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-27 15:36:52 +0100

    Update de.umd.js

--- assets/dist/js/lang/de.umd.js
+++ assets/dist/js/lang/de.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Sa11yLangEn = factory());
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Sa11yLangDe = factory());
 })(this, (function () { 'use strict';
 
     var de = {
