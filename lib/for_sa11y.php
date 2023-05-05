6d3693f79e1a45944691d703f96d9b8b7bfc9548
Thomas Skerbis <thomas.skerbis@mac.com>
2023-05-05 20:35:15 +0200

    Update for_sa11y.php

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -22,7 +22,7 @@ class for_sa11y
       <link rel="stylesheet" href="' . $addon->getAssetsUrl("dist/css/sa11y.min.css") . '"/>
       <script src="' . $addon->getAssetsUrl("dist/js/sa11y.umd.min.js") . '"></script>
       <script src="' . $addon->getAssetsUrl("dist/js/lang/".$lang['js'].".umd.js") . '"></script>
-       <script src="' . $addon->getAssetsUrl("dist/js/sa11y-custom-checks.umd.min.js") . '"></script>
+  
   <script>     
   Sa11y.Lang.addI18n(Sa11yLang'.$lang["setup"].'.strings);
   const sa11y = new Sa11y.Sa11y({
