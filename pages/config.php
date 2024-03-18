d5b9da7af00a0a6c821ee363da1c1f61265d1c44
Thomas Skerbis <thomas.skerbis@mac.com>
2024-03-18 12:22:43 +0100

    3.2.0

--- pages/config.php
+++ pages/config.php
@@ -1,20 +1,21 @@
 <?php
+$package = rex_addon::get('for_sa11y');
 $form = rex_config_form::factory('for_sa11y');
 $field = $form->addSelectField('active');
-$field->setLabel($this->i18n('for_sa11y_active_label'));
+$field->setLabel($package->i18n('for_sa11y_active_label'));
 $select = $field->getSelect();
 $select->setSize(1);
-$select->addOption($this->i18n('for_sa11y_active_true'), 'true');
-$select->addOption($this->i18n('for_sa11y_active_false'), 'false');
+$select->addOption($package->i18n('for_sa11y_active_true'), 'true');
+$select->addOption($package->i18n('for_sa11y_active_false'), 'false');
 
 if(rex::getUser()?->isAdmin())
 {  
     $field = $form->addInputField('text', 'root', null, ['class' => 'form-control']);
-    $field->setLabel($this->i18n('root'));
-    $field->getValidator()->add('notEmpty', $this->i18n('root_empty'));
+    $field->setLabel($package->i18n('root'));
+    $field->getValidator()->add('notEmpty', $package->i18n('root_empty'));
 }
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $this->i18n('for_sa11y_config'), false);
+$fragment->setVar('title', $package->i18n('for_sa11y_config'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
