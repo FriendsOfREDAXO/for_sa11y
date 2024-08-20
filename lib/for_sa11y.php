e079b1515cae225854a28400978fd93b14e6fbf3
Thomas Skerbis <thomas.skerbis@mac.com>
2024-08-20 15:13:33 +0200

    css link fix

--- lib/for_sa11y.php
+++ lib/for_sa11y.php
@@ -43,7 +43,7 @@ class Sa11y
 
             $addon = rex_addon::get('for_sa11y');
             $js = '      
-      <link rel="stylesheet" href="' . $addon->getAssetsUrl("dist/css/sa11y.min.css") . '"/>
+      <link rel="stylesheet" href="' . $addon->getAssetsUrl("vendor/sa11y/dist/css/sa11y.min.css") . '"/>
       <script src="' . $addon->getAssetsUrl("vendor/sa11y/dist/js/sa11y.umd.min.js") . '"></script>
       <script src="' . $addon->getAssetsUrl("vendor/sa11y/dist/js/lang/" . $lang['js'] . ".umd.js") . '"></script>
   
